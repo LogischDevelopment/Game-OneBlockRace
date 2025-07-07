968c4edd5f41efd38dbdcbd3d69ec3a2a4ad9fd2
Timo <timokeutgen@gmx.de>
2025-07-07 16:21:14 +0200

    Update PlayerMoveListener to reset player state upon void fall and adjust island boundary logic

--- src/main/java/tv/logisch/oneBlockRace/listener/PlayerMoveListener.java
+++ src/main/java/tv/logisch/oneBlockRace/listener/PlayerMoveListener.java
@@ -1,9 +1,11 @@
 package tv.logisch.oneBlockRace.listener;
 
 import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import tv.logisch.oneBlockRace.enums.GameState;
 import tv.logisch.oneBlockRace.manager.GameManager;
 
@@ -40,11 +42,27 @@ public class PlayerMoveListener implements Listener {
 
         if(t.getBlockY() < island.getBlockY() - 10) {
             e.getPlayer().teleport(island.clone().add(0.5, 1, 0.5));
+            e.getPlayer().setHealth(e.getPlayer().getAttribute(Attribute.MAX_HEALTH).getValue());
+            e.getPlayer().setFireTicks(0);
+            e.getPlayer().setFallDistance(0);
+
+            if(!GameManager.get().keepInventory()) {
+                e.getPlayer().getInventory().clear();
+                e.getPlayer().getInventory().setArmorContents(new ItemStack[0]);
+                e.getPlayer().getInventory().setHeldItemSlot(0);
+                e.getPlayer().setFoodLevel(20);
+                e.getPlayer().setSaturation(20);
+                e.getPlayer().setExp(0);
+                e.getPlayer().setLevel(0);
+                e.getPlayer().getActivePotionEffects().forEach(effect -> e.getPlayer().removePotionEffect(effect.getType()));
+            }
+            e.getPlayer().setAllowFlight(false);
+            e.getPlayer().setFlying(false);
             return;
         }
 
-        int x1 = island.getBlockX() - GameManager.get().islandWidth() / 2;
-        int x2 = island.getBlockX() + GameManager.get().islandWidth() / 2;
+        int x1 = island.getBlockX() - GameManager.get().islandWidth();
+        int x2 = island.getBlockX() + GameManager.get().islandWidth();
 
         if(t.getBlockX() < x1 || t.getBlockX() > x2) {
             e.getPlayer().setVelocity(e.getPlayer().getLocation().toVector().subtract(island.toVector()).normalize().multiply(-0.5));
