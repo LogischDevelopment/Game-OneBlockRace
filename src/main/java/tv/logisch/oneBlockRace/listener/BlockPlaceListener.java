df84fd6ca78e6fe40acba8ed2227359a31d53bc4
Timo <timokeutgen@gmx.de>
2025-07-07 16:21:06 +0200

    Adjust island boundary logic in BlockPlaceListener to fix block range calculation.

--- src/main/java/tv/logisch/oneBlockRace/listener/BlockPlaceListener.java
+++ src/main/java/tv/logisch/oneBlockRace/listener/BlockPlaceListener.java
@@ -30,8 +30,8 @@ public class BlockPlaceListener implements Listener {
         }
 
         int x = team.getIsland().getBlockX();
-        int x1 = x - GameManager.get().islandWidth()/2;
-        int x2 = x + GameManager.get().islandWidth()/2;
+        int x1 = x - GameManager.get().islandWidth();
+        int x2 = x + GameManager.get().islandWidth();
 
         if(x1 > x2) {
             int temp = x1;
