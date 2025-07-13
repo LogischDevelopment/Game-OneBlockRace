cedd2023a11ae8e63c00dd11c9684ec978121ba5
Timo <timokeutgen@gmx.de>
2025-07-13 17:14:35 +0200

    Refine JoinListener to set accurate spawn locations and ensure appropriate GameMode assignment

--- src/main/java/tv/logisch/oneBlockRace/listener/JoinListener.java
+++ src/main/java/tv/logisch/oneBlockRace/listener/JoinListener.java
@@ -2,12 +2,14 @@ package tv.logisch.oneBlockRace.listener;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import tv.logisch.oneBlockRace.OneBlockRace;
 import tv.logisch.oneBlockRace.enums.GameState;
 import tv.logisch.oneBlockRace.manager.GameManager;
 import tv.logisch.oneBlockRace.scoreboard.Scoreboard;
@@ -29,12 +31,13 @@ public class JoinListener implements Listener {
             Team team1 = GameManager.get().teamManager().getTeam(p);
             if(team1 != null) {
                 team1.teleportToIsland(e.getPlayer());
+                e.getPlayer().setGameMode(GameMode.SURVIVAL);
                 GameManager.get().itemManager().addPlayer(e.getPlayer());
                 Scoreboard.scoreboards.add(new Scoreboard(p));
                 return;
             }
 
-            p.setGameMode(org.bukkit.GameMode.SURVIVAL);
+            p.setGameMode(GameMode.SURVIVAL);
             p.setAllowFlight(false);
             p.setFlying(false);
             p.setHealth(20);
@@ -52,8 +55,8 @@ public class JoinListener implements Listener {
             team.teleportToIsland(p);
             GameManager.get().itemManager().addPlayer(e.getPlayer());
         } else {
-            e.getPlayer().setGameMode(org.bukkit.GameMode.ADVENTURE);
-            e.getPlayer().teleport(e.getPlayer().getWorld().getSpawnLocation());
+            e.getPlayer().setGameMode(GameMode.ADVENTURE);
+            e.getPlayer().teleport(e.getPlayer().getWorld().getSpawnLocation().clone().add(0.5, 1, 0.5));
         }
 
         Scoreboard.scoreboards.add(new Scoreboard(p));
