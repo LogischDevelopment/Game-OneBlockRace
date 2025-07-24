4900bb51f4667455cfd908a20a310f64287374b7
Timo <timokeutgen@gmx.de>
2025-07-25 01:10:24 +0200

    Restrict block interactions based on game mode and PVP state
    - Prevent block breaking and placing for players in Creative mode.
    - Added timed removal logic for specific blocks (BRICKS, COBWEB) during the PVP phase.
    - Implemented TNT block replacement with a primed TNT entity and custom fuse time during PVP.

--- src/main/java/tv/logisch/oneBlockRace/listener/BlockBreakListener.java
+++ src/main/java/tv/logisch/oneBlockRace/listener/BlockBreakListener.java
@@ -1,5 +1,6 @@
 package tv.logisch.oneBlockRace.listener;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -12,6 +13,7 @@ public class BlockBreakListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
+        if(e.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
 
         if(!GameManager.get().state().equals(GameState.RUNNING)) {
             e.setCancelled(true);
--- src/main/java/tv/logisch/oneBlockRace/listener/BlockPlaceListener.java
+++ src/main/java/tv/logisch/oneBlockRace/listener/BlockPlaceListener.java
@@ -1,8 +1,14 @@
 package tv.logisch.oneBlockRace.listener;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import tv.logisch.oneBlockRace.OneBlockRace;
 import tv.logisch.oneBlockRace.enums.GameState;
 import tv.logisch.oneBlockRace.manager.GameManager;
 import tv.logisch.oneBlockRace.scoreboard.Scoreboard;
@@ -12,6 +18,31 @@ public class BlockPlaceListener implements Listener {
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
+        if(e.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
+
+        if(GameManager.get().state().equals(GameState.PVP)) {
+            if (e.getBlockPlaced().getType().equals(Material.BRICKS)) {
+                Bukkit.getScheduler().runTaskLater(OneBlockRace.instance(), () -> {
+                    e.getBlockPlaced().setType(Material.AIR);
+                }, 7*20L);
+                return;
+            }
+            if(e.getBlockPlaced().getType().equals(Material.TNT)) {
+                e.getBlockPlaced().setType(Material.AIR);
+
+                Location loc = e.getBlockPlaced().getLocation().add(0.5, 0, 0.5);
+                TNTPrimed tnt = (TNTPrimed) loc.getWorld().spawn(loc, TNTPrimed.class);
+                tnt.setSource(e.getPlayer());
+                tnt.setFuseTicks(80); // 4 seconds
+                return;
+            }
+            if(e.getBlockPlaced().getType().equals(Material.COBWEB)) {
+                Bukkit.getScheduler().runTaskLater(OneBlockRace.instance(), () -> {
+                    e.getBlockPlaced().setType(Material.AIR);
+                }, 16*20L);
+                return;
+            }
+        }
 
         if(!GameManager.get().state().equals(GameState.RUNNING)) {
             e.setCancelled(true);
